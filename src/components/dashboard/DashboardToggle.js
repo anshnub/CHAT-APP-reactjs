f2513ce2ab1a7fb332baa9841122032178bb48e8
ansh tyagi <anshtyagi1818@gmail.com>
2023-05-13 16:53:55 +0530

    linked google n facebook to one acnt

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import { Button, Drawer, Icon } from 'rsuite'
-import { useModalState } from '../../misc/custom-hooks'
+import React, { useCallback } from 'react'
+import { Alert, Button, Drawer, Icon } from 'rsuite'
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
 import Dashboard from '.'
+import { auth } from '../../misc/firebase'
 
 
 const DashboardToggle = () => {
-    const {isOpen , close, open} = useModalState()
+    const {isOpen , close, open} = useModalState();
+    const isMobile = useMediaQuery('(max-width: 992px)');
+
+    const onSignOut = useCallback (()=>{
+      auth.signOut();
+      Alert.info('Signed out',4000)
+      close();
+    }, [close])
   return (
     <>
    <Button block color="blue" onClick={open} >
@@ -16,9 +24,9 @@ const DashboardToggle = () => {
 
 
    </Button>
-   <Drawer show={isOpen} onHide={close} placement="left" >
+   <Drawer full={isMobile} show={isOpen} onHide={close} placement="left" >
 
-   <Dashboard />
+   <Dashboard onSignOut={onSignOut} />
 
    </Drawer>
 
